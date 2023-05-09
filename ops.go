0222c2080f9c83e01d5d8328a12ef1b01dd7db8a
平强强 <pingqq@g-bits.com>
2023-05-09 10:21:43 +0800

    增加类型

--- ops.go
+++ ops.go
@@ -1,6 +1,6 @@
 package anet
 
-type distGroup struct {
+type DistGroup struct {
 	AgentId  string   `json:"agent_id"`  // agent id
 	DistList []string `json:"dist_list"` // dist 列表
 }
@@ -9,7 +9,7 @@ type OpsFullVersionSyncArgs struct {
 	SignUrl   string      `json:"sign_url"`   // oss 下载地址
 	LogPath   string      `json:"log_path"`   // 日志路径
 	VerType   int         `json:"ver_type"`   // 版本类型
-	DistGroup []distGroup `json:"dist_group"` // 区组组
+	DistGroup []DistGroup `json:"dist_group"` // 区组组
 }
 
 type OpsFullVersionSyncRep struct {
